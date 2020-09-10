f71ba2f502c09f515154a0bbe47de0d96138c23c
savage12138 <yida12138@gmail.com>
2020-09-09 20:17:32 -0400

    mew

---
+++ mew/mew_parameters.m
@@ -0,0 +1,53 @@
+% This file contains all the necessary parameters
+% for mew.m script.
+
+% rotation parameters (degrees)
+x_ang = 90;
+y_ang = 20;
+z_ang = 0;
+% x_ang = 90;
+% y_ang = -20;
+% z_ang = 0;
+% x_ang = 90;
+% y_ang = -200;
+% z_ang = 0;
+% translation parameters
+Tx = 0;
+Ty = -7;
+Tz = 3;
+% Tx = 0;
+% Ty = -8;
+% Tz = 3;
+% Tx = 5;
+% Ty = -5;
+% Tz = 5;
+% material color
+Red = 245;
+Green = 204;
+Blue = 220;
+% lighting position
+light_position = [4 6 3];
+% light_position = [4 6 10];
+% light_position = [4 6 10];
+
+% camera position
+Cx = 0;
+Cy = 1;
+Cz = 7.5;
+% Cx = 0;
+% Cy = 3;
+% Cz = 6.5;
+% Cx = 5;
+% Cy = 3;
+% Cz = 12;
+% look at position is where camera is pointing
+% camera look at position
+camera_look_at = [0 0 4];
+% camera_look_at = [0 1 4];
+% field of view angle in degrees
+FOV_ang = 90;
+% near plane
+z_near = 1;
+% far plane
+z_far = 30;
+
