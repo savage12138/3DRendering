3a5792a6c6aae6145208f9a3869748c82aba90d3
savage12138 <yida12138@gmail.com>
2020-09-09 20:17:56 -0400

    mewtwo

---
+++ mewtwo/mewtwo_parameters.m
@@ -0,0 +1,41 @@
+% This file contains all the necessary parameters
+% for mewtwo.m script.
+
+% rotation parameters (degrees)
+x_ang = 90;
+y_ang = 45;
+z_ang = 0;
+% x_ang = 90;
+% y_ang = -35;
+% z_ang = 0;
+% translation parameters
+Tx = 0;
+Ty = 0;
+Tz = 60;
+% Tx = 0;
+% Ty = 5;
+% Tz = 60;
+% material color
+Red = 249;
+Green = 219;
+Blue = 227;
+% lighting position
+light_position = [50 50 400];
+% camera position
+Cx = 0;
+Cy = 15;
+Cz = 75;
+% Cx = 0;
+% Cy = 25;
+% Cz = 75;
+% look at position is where camera is pointing
+% camera look at position
+camera_look_at = [0 -8 0];
+% camera_look_at = [0 -18 0];
+% field of view angle in degrees
+FOV_ang = 90;
+% near plane
+z_near = 1;
+% far plane
+z_far = 500;
+
